5422dfff3f26ba4d0b675fb317a8058f704ddc5d
nanadouk <anna.doukmak@gmail.com>
2018-05-16 21:12:28 +0200

    updated OutputScriptCreator for OpReturn script

--- src/main/java/ch/bfh/blk2/bitcoin/blockchain2database/Dataclasses/OuputScriptCreator.java
+++ src/main/java/ch/bfh/blk2/bitcoin/blockchain2database/Dataclasses/OuputScriptCreator.java
@@ -55,7 +55,7 @@ public class OuputScriptCreator {
 				if (script.isSentToMultiSig()){
 					OutputScript outputScript;
 					try{
-						return new MultiSigScript(script, scriptSize, txId, txIndex);
+						outputScript = new MultiSigScript(script, scriptSize, txId, txIndex);
 					} catch (IllegalArgumentException e){
 						logger.debug("Non standard SENT TO MULTI SIG output: " + e.toString() );
 						logger.debug("The script looks like so: " + script.toString());
@@ -66,17 +66,16 @@ public class OuputScriptCreator {
 				}
 
 				// OP_RETURN w/ data >80 Byte is not relayed by core client, thus not a standard transaction...
-				if (script.isOpReturn() )
-					if (script.getChunks().size() == 2) {
-						if (script.getChunks().get(1).data != null && script.getChunks().get(1).data.length <= MAX_OP_RETURN_SIZE)
-							return new OPReturnScript(script, scriptSize, txId, txIndex);
-						else
-							return new OtherScript(script, scriptSize, txId, txIndex, ScriptType.OUT_OP_RETURN_SPEC);
-					} else
-							return new OtherScript(script, scriptSize, txId, txIndex, ScriptType.OUT_OP_RETURN_SPEC);
+				if (script.isOpReturn() ) {
+						if (script.getChunks().size() == 2) {
+							if (script.getChunks().get(1).data != null && script.getChunks().get(1).data.length <= MAX_OP_RETURN_SIZE)
+								return new OPReturnScript(script, scriptSize, txId, txIndex);
+							else
+								return new OtherScript(script, scriptSize, txId, txIndex, ScriptType.OUT_OP_RETURN_SPEC);
+						} else
+								return new OtherScript(script, scriptSize, txId, txIndex, ScriptType.OUT_OP_RETURN_SPEC);
 
-
-
+				}
 
 			} catch (IllegalArgumentException e) {
 				logger.debug("There was an error when trying to detect the script type for the following scrip: "
