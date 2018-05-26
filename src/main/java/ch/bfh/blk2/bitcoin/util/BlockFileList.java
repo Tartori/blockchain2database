e214444aef33e0f5447d34d3d07341eda749fc16
nanadouk <anna.doukmak@gmail.com>
2018-05-26 14:37:32 +0200

    Deleted unneeded imports.

--- src/main/java/ch/bfh/blk2/bitcoin/util/BlockFileList.java
+++ src/main/java/ch/bfh/blk2/bitcoin/util/BlockFileList.java
@@ -1,17 +1,13 @@
 package ch.bfh.blk2.bitcoin.util;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FilenameFilter;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Properties;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
